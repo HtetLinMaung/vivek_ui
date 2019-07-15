b7aa03d0b05e29015bc2cf50f4f1379ec9bfd4c2
htet_lin_maung <htet.lin.maung.@fusionsol.com>
2019-07-15 10:07:05 +0700

    adding cards to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,76 @@
 <template>
-  <HelloWorld />
+  <v-container grid-list-xl>
+    <v-breadcrumbs :items="items" divider=">"></v-breadcrumbs>
+
+    <v-layout wrap v-for="home_item in home_items" :key="home_item">
+      <v-flex md2 v-for="cards in home_item" :key="cards.title">
+        <v-card class="rounded">
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{ cards.title }}</h3>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+      <v-flex grow></v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+export default {
+  data: () => ({
+    items: [
+      {
+        text: "Home",
+        disabled: false,
+        href: "/"
+      }
+    ],
+    home_items: [
+      [
+        {
+          icon: "",
+          title: "Meditation Hall"
+        },
+        {
+          icon: "",
+          title: "Music"
+        },
+        {
+          icon: "",
+          title: "Case Study"
+        },
+        {
+          icon: "",
+          title: "Community"
+        },
+        {
+          icon: "",
+          title: "Course Update"
+        }
+      ],
+      [
+        {
+          icon: "",
+          title: "Teacher Bio"
+        },
+        {
+          icon: "",
+          title: "Feedback Box"
+        },
+        {
+          icon: "",
+          title: "Registered User Info"
+        }
+      ]
+    ]
+  })
+};
 </script>
+
+<style lang="scss">
+.rounded {
+  border-radius: 1rem;
+}
+</style>
